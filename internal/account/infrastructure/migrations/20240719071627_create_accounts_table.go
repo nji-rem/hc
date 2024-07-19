e51367034509b3f39cd109bd64f933fbd448a4f7
Merijn <merijnkruithof@hotmail.com>
2024-07-19 09:49:55 +0200

    add technical partitioned root directory for account bounded context

--- internal/account/infrastructure/migrations/20240719071627_create_accounts_table.go
+++ internal/account/infrastructure/migrations/20240719071627_create_accounts_table.go
@@ -10,8 +10,22 @@ func init() {
 	goose.AddMigrationContext(upCreateAccountsTable, downCreateAccountsTable)
 }
 
+const Sql = `
+	CREATE TABLE accounts (
+	    id INTEGER PRIMARY KEY,
+	    username VARCHAR(255) NOT NULL,
+	    password VARCHAR(255) NOT NULL,
+	    look VARCHAR(255) NOT NULL,
+	    motto VARCHAR(255)
+	);
+`
+
 func upCreateAccountsTable(ctx context.Context, tx *sql.Tx) error {
-	// This code is executed when the migration is applied.
+	_, err := tx.Exec(Sql)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
